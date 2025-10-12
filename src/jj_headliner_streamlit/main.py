b476644165679d7a6d6eb32e514bf77495e315a2
ihor <Ihor.Halaniuk@gmail.com>
2025-10-12 17:51:18 +0300

    0. Import dvc data set from remote.

--- src/jj_headliner_streamlit/main.py
+++ src/jj_headliner_streamlit/main.py
@@ -34,7 +34,13 @@ if not DATA_FILE.exists():
             f.write(st.secrets[GOOGLE_APPLICATION_CREDENTIALS_SECRET_KEY])
     github_token = st.secrets[GITHUB_TOKEN_SECRET_KEY]
     with (
-        dvc.api.open(REMOTE_FILE, get_github_repo_url(github_token), remote="gcp-gs") as src,
+        dvc.api.open(
+            REMOTE_FILE,
+            get_github_repo_url(github_token),
+            remote_config={
+                "credentialpath": GOOGLE_APPLICATION_CREDENTIALS,
+            },
+        ) as src,
         DATA_FILE.open("wb") as dst,
     ):
         shutil.copyfileobj(src, dst)
