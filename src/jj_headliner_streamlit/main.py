109b1bdd3755b6ba1807370199cdc970fab2be59
ihor <Ihor.Halaniuk@gmail.com>
2025-10-12 18:52:26 +0300

    0. Import dvc data set from remote.

--- src/jj_headliner_streamlit/main.py
+++ src/jj_headliner_streamlit/main.py
@@ -31,7 +31,9 @@ if not DATA_FILE.exists():
     if not GOOGLE_APPLICATION_CREDENTIALS.exists():
         GOOGLE_APPLICATION_CREDENTIALS.parent.mkdir(parents=True, exist_ok=True)
         with GOOGLE_APPLICATION_CREDENTIALS.open("w") as f:
-            f.write(st.secrets[GOOGLE_APPLICATION_CREDENTIALS_SECRET_KEY].replace("\\n", "\\\\n"))
+            gcp_creds = st.secrets[GOOGLE_APPLICATION_CREDENTIALS_SECRET_KEY]
+            print(f"{type(gcp_creds)=}")
+            f.write(st.secrets[GOOGLE_APPLICATION_CREDENTIALS_SECRET_KEY])
     print(f"[!!!!]Writing {GOOGLE_APPLICATION_CREDENTIALS}")
     print(GOOGLE_APPLICATION_CREDENTIALS.read_text()[:200])
     github_token = st.secrets[GITHUB_TOKEN_SECRET_KEY]
