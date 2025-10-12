8b0d74fb0d342661b6324443e8b55a7c5e4d8d34
ihor <Ihor.Halaniuk@gmail.com>
2025-10-12 17:24:06 +0300

    0. Import dvc data set from remote.

--- src/jj_headliner_streamlit/main.py
+++ src/jj_headliner_streamlit/main.py
@@ -1,15 +1,18 @@
 from __future__ import annotations
 
 import logging
-import subprocess
+import shutil
 from typing import TYPE_CHECKING, Final
 
+import dvc
+import dvc.api
 import pandas as pd
 import streamlit as st
 
 from jj_headliner_streamlit.config import (
     DD,
     DVC_IMPORT_PATH,
+    DVC_IMPORT_REPO,
     GOOGLE_APPLICATION_CREDENTIALS,
     GOOGLE_APPLICATION_CREDENTIALS_SECRET_KEY,
 )
@@ -23,11 +26,13 @@ LOG = logging.getLogger(__name__)
 DATA_FILE: Final[Path] = DD / "99_test" / "headlines" / "test-data-set.parquet"
 
 if not DATA_FILE.exists():
+    DATA_FILE.parent.mkdir(parents=True, exist_ok=True)
     if not GOOGLE_APPLICATION_CREDENTIALS.exists():
         GOOGLE_APPLICATION_CREDENTIALS.parent.mkdir(parents=True, exist_ok=True)
         with GOOGLE_APPLICATION_CREDENTIALS.open("w") as f:
             f.write(st.secrets[GOOGLE_APPLICATION_CREDENTIALS_SECRET_KEY])
-    subprocess.run(["uv", "run", "dvc", "update", DVC_IMPORT_PATH], check=True)  # noqa: S603, S607
+    with dvc.api.open(DVC_IMPORT_PATH, DVC_IMPORT_REPO, remote="gcp-gs") as src, DATA_FILE.open("wb") as dst:
+        shutil.copyfileobj(src, dst)
 
 st.title("Hello Streamlit!")
 
